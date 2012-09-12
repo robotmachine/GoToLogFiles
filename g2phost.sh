c5ec555e8152cb5aca904e3abdccb37530f75121
Brian Carter <robotmachine@gmail.com>
2012-09-11 19:41:01 -0700

    Will now add the install.log

--- g2phost.sh
+++ g2phost.sh
@@ -5,7 +5,8 @@
 	rsync -a --exclude="MobileDevice" ~/Library/Logs/CrashReporter/* ~/Desktop/GoToMyPC_Host_Logs/CrashReporterUser/; \
 	rsync -a /Library/Logs/DiagnosticReports/* ~/Desktop/GoToMyPC_Host_Logs/CrashReporterSystem/; \
 # Copy the system log to the temporary folder.
-	rsync -a /Private/Var/Log/system.log* ~/Desktop/GoToMyPC_Host_Logs/SystemLog; \
+	rsync -a /private/var/log/system.log* ~/Desktop/GoToMyPC_Host_Logs/SystemLog; \
+	rsync -a /private/var/log/install.log ~/Desktop/GoToMyPC_Host_Logs/SystemLog; \
 # Copy Endpoint Logs to the temporary folder.
 	rsync -a /Library/Logs/com.citrixonline.GoToMyPC/* ~/Desktop/GoToMyPC_Host_Logs/Endpoint_Logs; \
 # Get a list of running applications and installed applications.
