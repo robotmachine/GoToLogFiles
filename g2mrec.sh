9316a8954002e0f78379acef6ab76317ba2a3f39
Brian Carter <robotmachine@gmail.com>
2013-02-18 12:40:07 -0800

    Fixed recording log for 10.7 and up.

--- g2mrec.sh
+++ g2mrec.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # Create a temporary folder if it does not already exist.
-	if [ ! -d "~/Desktop/GoToMeeting_Logs" ]; then mkdir ~/Desktop/GoToMeeting_Logs; fi;\
+	if [ ! -d "~/Desktop/GoToMeeting_Recording_Logs" ]; then mkdir ~/Desktop/GoToMeeting_Recording_Logs; fi;\
 # Copy CrashReporter files to a temporary folder.
-	rsync -a --exclude="MobileDevice" ~/Library/Logs/CrashReporter/* ~/Desktop/GoToMeeting_Logs/CrashReporterUser/; \
-	rsync -a /Library/Logs/DiagnosticReports/* ~/Desktop/GoToMeeting_Logs/CrashReporterSystem/; \
+	rsync -a --exclude="MobileDevice" ~/Library/Logs/CrashReporter/* ~/Desktop/GoToMeeting_Recording_Logs/CrashReporterUser/; \
+	rsync -a /Library/Logs/DiagnosticReports/* ~/Desktop/GoToMeeting_Recording_Logs/CrashReporterSystem/; \
 # Copy the system log to the temporary folder.
-	rsync -a /Private/Var/Log/system.log* ~/Desktop/GoToMeeting_Logs/SystemLog; \
+	rsync -a /Private/Var/Log/system.log* ~/Desktop/GoToMeeting_Recording_Logs/SystemLog; \
 # Copy Endpoint and Recording Manager Logs to the temporary folder.
-	rsync -a ~/Library/Logs/com.citrixonline.GoToMeeting/* ~/Desktop/GoToMeeting_Logs/Endpoint_Logs; \
-	if [ -d ~/Library/Logs/com.citrixonline.GoToMeeting_Recording_Manager ]; then rsync -a ~/Library/Logs/com.citrixonline.GoToMeeting_Recording_Manager/* ~/Desktop/GoToMeeting_Logs/Recording_Manager; elif [ -d ~/Library/Logs/com.citrixonline.Mac.GoToMeeting.RecordingManager ]; then rsync -a ~/Library/Logs/com.citrixonline.Mac.GoToMeeting.RecordingManager/* ~/Desktop/GoToMeeting_Logs/Recording_Manager;  fi \
+	rsync -a ~/Library/Logs/com.citrixonline.GoToMeeting/* ~/Desktop/GoToMeeting_Recording_Logs/Endpoint_Logs; \
+	if [ -d ~/Library/Logs/com.citrixonline.GoToMeeting_Recording_Manager ]; then rsync -a ~/Library/Logs/com.citrixonline.GoToMeeting_Recording_Manager/* ~/Desktop/GoToMeeting_Recording_Logs/Recording_Manager; elif [ -d ~/Library/Logs/com.citrixonline.Mac.GoToMeeting.RecordingManager ]; then rsync -a ~/Library/Logs/com.citrixonline.Mac.GoToMeeting.RecordingManager/* ~/Desktop/GoToMeeting_Recording_Logs/Recording_Manager;  fi \
 # Copy unconverted video and audio files.
-	if [ -f ~/Documents/Recordings/*.temp_audio ]; then rsync -a ~/Documents/Recordings/*.temp_{audio,video} ~/Desktop/GoToMeeting_Logs/Unconverted_Temp_Files; fi \
+	if [ -d ~/Documents/Recordings ]; then rsync -a ~/Documents/Recordings/*.temp_{audio,video} ~/Desktop/GoToMeeting_Recording_Logs/Unconverted_Temp_Files; fi \
 # Looks in the settings plist for any other directories for the temp files to be in and copies those, too.
-THE_PATH=$(defaults -currentHost read com.citrixonline.GoToMeeting | grep RecordingPath | sed 's/    RecordingPath = //g' | sed 's/\;//g' | sed 's/\"//g') ; if [ -f "$THE_PATH"/*.temp_audio ]; then rsync -a $THE_PATH/*.temp_{audio,video} ~/Desktop/GoToMeeting_Logs/Unconverted_Temp_Files_Alternate; fi
+THE_PATH=$(defaults -currentHost read com.citrixonline.GoToMeeting | grep RecordingPath | sed 's/    RecordingPath = //g' | sed 's/\;//g' | sed 's/\"//g') ; if [ -f "$THE_PATH"/*.temp_audio ]; then rsync -a $THE_PATH/*.temp_{audio,video} ~/Desktop/GoToMeeting_Recording_Logs/Unconverted_Temp_Files_Alternate; fi
 # Get a list of running applications and installed applications.
-	ps aux > ~/Desktop/GoToMeeting_Logs/Processes.txt; \
-	system_profiler > ~/Desktop/GoToMeeting_Logs/System_Profiler.txt; \
+	ps aux > ~/Desktop/GoToMeeting_Recording_Logs/Processes.txt; \
+	system_profiler > ~/Desktop/GoToMeeting_Recording_Logs/System_Profiler.txt; \
 # Create a Gzipped Tar of all of the folders on the desktop.
-	tar -czf ~/Desktop/GoToMeeting_Logs.tgz -C ~/Desktop/ GoToMeeting_Logs ; \
+	tar -czf ~/Desktop/GoToMeeting_Recording_Logs.tgz -C ~/Desktop/ GoToMeeting_Recording_Logs ; \
 # Remove temporary folder.
-	rm -rf ~/Desktop/GoToMeeting_Logs
+	rm -rf ~/Desktop/GoToMeeting_Recording_Logs
