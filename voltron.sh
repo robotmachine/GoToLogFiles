568c9f946e569218093eae7fb8dadffac1392837
Brian Carter <robotmachine@gmail.com>
2013-11-18 21:23:11 -0800

    Script creates a combined script suitable for adding in to the Automator workflow.

---
+++ voltron.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# This takes all of the different scripts and puts them together to be dropped in to Automator.
+# Written by Brian Carter
+
+# Using Unix epoch for temp filename.
+VOLTRON=Combined_$(date +%s).txt
+echo "#!/bin/bash" > $VOLTRON
+echo "" >> $VOLTRON
+echo "if [ $@ = "GoToMyPC_Host" ]; then" >> $VOLTRON
+echo "" >> $VOLTRON
+cat g2phost.sh >> $VOLTRON
+echo "" >> $VOLTRON
+echo "elif [ $@ = "GoToMyPC_Client" ]; then" >> $VOLTRON
+echo "" >> $VOLTRON
+cat g2pclient.sh >> $VOLTRON
+echo "" >> $VOLTRON
+echo "elif [ $@ = "GoToMeeting" ]; then" >> $VOLTRON
+echo "" >> $VOLTRON
+cat g2m.sh >> $VOLTRON
+echo "elif [ $@ = "GoToMeeting_Recording_Logs" ]; then" >> $VOLTRON
+echo "" >> $VOLTRON
+cat g2mrec.sh >> $VOLTRON
+echo "" >> $VOLTRON
+echo "elif [ $@ = "GoToAssist_Corporate" ]; then" >> $VOLTRON
+echo "" >> $VOLTRON
+cat g2ac.sh >> $VOLTRON
+echo "" >> $VOLTRON
+echo "elif [ $@ = "GoToAssist_Remote_Support" ]; then" >> $VOLTRON
+echo "" >> $VOLTRON
+cat g2ars.sh >> $VOLTRON
+echo "" >> $VOLTRON
+echo "else" >> $VOLTRON
+echo "	exit" >> $VOLTRON
+echo "fi" >> $VOLTRON
