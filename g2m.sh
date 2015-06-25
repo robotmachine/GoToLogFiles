b1cc355f555d0a45a9f1909566ca15bfa9edabd3
Brian Carter <robotmachine@gmail.com>
2015-06-25 14:53:26 -0700

    Updated system info logging to include $(diskutil list) and $(df -H) to determine disk type information for troubleshooting recordings.

--- g2m.sh
+++ g2m.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/bin/bash
 # This script makes a compressed archive of the current user's desktop of log files, system diagnostics, and other Citrix Online related items.
 # Written by Brian Carter & Kyle Halversen
 #cutthis elif [ $@ = "GoToMeeting" ]; then
@@ -43,7 +43,9 @@ trap "cleanup" EXIT
 	sample "GoToMeeting Recording Manager" > $TEMPDIR/Sample/GoToMeetingRecMgr_Sample.txt 2>&1
 
 # Get a list of running applications and installed applications.
-	ps aux > $TEMPDIR/Processes.txt
+	ps > $TEMPDIR/Processes.txt
+	diskutil list >> $TEMPDIR/Disk_Info.txt
+	df -H >> $TEMPDIR/Disk_Info.txt
 	system_profiler SPApplicationsDataType >> $TEMPDIR/System_Profiler.txt
 	system_profiler SPSoftwareDataType >> $TEMPDIR/System_Profiler.txt
 	system_profiler SPHardwareDataType >> $TEMPDIR/System_Profiler.txt
