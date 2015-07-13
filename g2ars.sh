fc303a56f7e5688b2ea631b301741cdf453007d9
Brian Carter <robotmachine@gmail.com>
2015-07-13 15:39:15 -0700

    Added plist to logs.

--- g2ars.sh
+++ g2ars.sh
@@ -12,4 +12,14 @@ source start.sh
 	if [ -d ~/Library/Logs/com.citrixonline.g2ax.customer ]; then logcomment "GoToAssist Remote Support: Logs" ; rsync -av ~/Library/Logs/com.citrixonline.g2ax.customer $TEMPDIR/Pre_Build_403_Logs/ >> $LOGFILE 2>&1 ; fi
 	if [ -d ~/Library/Logs/com.citrixonline.g2ax.expert ]; then logcomment "GoToAssist Remote Support: Logs" ; rsync -av ~/Library/Logs/com.citrixonline.g2ax.expert $TEMPDIR/Pre_Build_403_Logs/ >> $LOGFILE 2>&1 ; fi
 
+# Copy preferences to a text file.
+	if ls ~/Library/Preferences/com.citrixonline.g2ax* 1> /dev/null 2>&1 ; then
+		mkdir $TEMPDIR/Plist
+		for FILE in ~/Library/Preferences/com.citrixonline.g2ax* ; do
+			defaults read $FILE > $TEMPDIR/Plist/$(echo "$FILE" | awk -F \/ '{ print $NF }').txt
+		done
+	else
+		logcomment "GoToAssist Remote Support Plist .:. No plist files found."
+	fi
+
 source finish.sh
